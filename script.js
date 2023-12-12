cb4dd34b5c42e783e1eeb4513ea4afdd1682ac40
techwizz1 <142192839+techwizz1@users.noreply.github.com>
2023-12-11 22:13:15 -0500

    0

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector(".menu-icon");
+const container = document.querySelector(".container");
+
+menuIcon.addEventListener("click", () =>{
+    container.classList.toggle(change);
+
+});
+
